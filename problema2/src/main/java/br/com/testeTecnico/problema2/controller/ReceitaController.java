ecb018a69210cbe49cf2e4281bd2c6cc7ce569fd
eduardo-c4pitani <eduardocapitani22@gmail.com>
2023-10-27 09:15:56 -0300

    Ajuste de projeto

--- problema2/src/main/java/br/com/testeTecnico/problema2/controller/ReceitaController.java
+++ problema2/src/main/java/br/com/testeTecnico/problema2/controller/ReceitaController.java
@@ -7,10 +7,14 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ReceitaController {
+
     @GetMapping("/buscar-receitas")
     public String buscarReceitas(@RequestParam String prato) {
+
         RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "https://forkify-api.herokuapp.com/api/search?q=" + prato;
         return restTemplate.getForObject(apiUrl, String.class);
+
     }
+
 }
