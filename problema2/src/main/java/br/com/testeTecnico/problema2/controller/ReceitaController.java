07b733395ff8bfaa31ed17401955f068e5f14eae
eduardo-c4pitani <eduardocapitani22@gmail.com>
2023-10-31 16:34:02 -0300

    Ajuste no código

--- problema2/src/main/java/br/com/testeTecnico/problema2/controller/ReceitaController.java
+++ problema2/src/main/java/br/com/testeTecnico/problema2/controller/ReceitaController.java
@@ -1,6 +1,7 @@
 package br.com.testeTecnico.problema2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,8 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ReceitaController {
 
-    @GetMapping("/buscar-receitas")
-    public String buscarReceitas(@RequestParam String prato) {
+    @GetMapping("/buscar-receitas/{prato}")
+    public String buscarReceitas(@PathVariable("prato") String prato) {
 
         RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "https://forkify-api.herokuapp.com/api/search?q=" + prato;
